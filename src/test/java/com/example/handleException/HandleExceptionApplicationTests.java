26a7aa207c6e1190b8d3b21f04dd870e67f09a73
GyuYoung Park <gy971224@gmail.com>
2021-10-27 13:00:37 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/handleException/HandleExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handleException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
